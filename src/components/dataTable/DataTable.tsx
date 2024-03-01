f807a1fd773d427b15a35a8dc0c8677ba5993264
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-03-01 07:41:01 +0530

    Made datatable reusable

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -1,35 +1,85 @@
-import { DataGrid, GridRowsProp, GridColDef, GridToolbar } from '@mui/x-data-grid';
-import "./dataTable.scss"
-const rows: GridRowsProp = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-];
+import {
+    DataGrid,
+    GridColDef,
+    GridToolbar,
+  } from "@mui/x-data-grid";
+  import "./dataTable.scss";
+  import { Link } from "react-router-dom";
+  // import { useMutation, useQueryClient } from "@tanstack/react-query";
+  
+  type Props = {
+    columns: GridColDef[];
+    rows: object[];
+  };
 
-const columns: GridColDef[] = [
-    { field: 'col1', headerName: 'Column 1', width: 150, editable: true},
-    { field: 'col2', headerName: 'Column 2', width: 150, editable: true},
-];
-
-function DataTable() {
+  const DataTable = (props: Props) => {
+  
+    // TEST THE API
+  
+    // const queryClient = useQueryClient();
+    // // const mutation = useMutation({
+    // //   mutationFn: (id: number) => {
+    // //     return fetch(`http://localhost:8800/api/${props.slug}/${id}`, {
+    // //       method: "delete",
+    // //     });
+    // //   },
+    // //   onSuccess: ()=>{
+    // //     queryClient.invalidateQueries([`all${props.slug}`]);
+    // //   }
+    // // });
+  
+    const handleDelete = (id: number) => {
+      //delete the item
+      // mutation.mutate(id)
+    };
+  
+    const actionColumn: GridColDef = {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className="action">
+            <Link to={`/${params.row.id}`}>
+              <img src="/view.svg" alt="" />
+            </Link>
+            <div className="delete" onClick={() => handleDelete(params.row.id)}>
+              <img src="/delete.svg" alt="" />
+            </div>
+          </div>
+        );
+      },
+    };
+  
     return (
-
-        <div className="dataTable">
-            <DataGrid className='dataGrid' rows={rows} columns={columns}
-            slots={{toolbar:GridToolbar}}
-            slotProps={{
-                toolbar: {
-                    showQuickFilter: true,
-                    quickFilterProps: {debounceMs: 500}
-                }
-            }}
-            disableRowSelectionOnClick
-            disableColumnFilter
-            disableDensitySelector
-            disableColumnSelector
-            />
-        </div>
-    )
-}
-
-export default DataTable
+      <div className="dataTable">
+        <DataGrid
+          className="dataGrid"
+          rows={props.rows}
+          columns={[...props.columns, actionColumn]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+          disableColumnFilter
+          disableDensitySelector
+          disableColumnSelector
+        />
+      </div>
+    );
+  };
+  
+  export default DataTable;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,6 +1,21 @@
+// const rows: GridRowsProp = [
+//   //   { id: 1, col1: 'Hello', col2: 'World', status: true },
+//   //   { id: 2, col1: 'DataGridPro', col2: 'is Awesome', status: false },
+//   //   { id: 3, col1: 'MUI', col2: 'is Amazing', status: true },
+//   // ];
+
+
+import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
+import { userRows } from "../../data";
 
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150, editable: true},
+  { field: 'status', headerName: 'Status', width: 100, type:'boolean'},
+  { field: 'col2', headerName: 'Column 2', width: 150, editable: true},
+  { field: 'col2', headerName: 'Column 2', width: 150, editable: true},
+];
 function Users() {
   return (
     <div className='users'>
@@ -8,7 +23,7 @@ function Users() {
         <h1>Users</h1>
         <button>Add New User</button>
         <div className="table">
-          <DataTable />
+          <DataTable columns={columns} rows={userRows} />
         </div>
       </div>
     </div>
