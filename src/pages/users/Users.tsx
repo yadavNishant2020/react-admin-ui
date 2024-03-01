6e5ddd5e5c35b4db64bedea89ee3c4b9bb261d36
Nishant Singh Yadav <nishant@ditinustechnology.com>
2024-03-01 10:46:23 +0530

    Revert "Added data-grid"
    This reverts commit 19c049c3b8ddb9acbc3252bd2cb6cc5e35975196.

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,17 +1,8 @@
-import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
 
 function Users() {
   return (
-    <div className='users'>
-      <div className="info">
-        <h1>Users</h1>
-        <button>Add New User</button>
-        <div className="table">
-          <DataTable />
-        </div>
-      </div>
-    </div>
+    <div className='user'>Users</div>
   )
 }
 
