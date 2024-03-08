cc1f0a51a96b99ba385ba5bf4970ebf4ee674f46
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-03-08 07:35:41 +0530

    Add AddModal component and styling to Products and Users pages

--- src/components/addModal/AddModal.tsx
+++ src/components/addModal/AddModal.tsx
@@ -1,11 +1,36 @@
+import { GridColDef } from "@mui/x-data-grid"
 import "./addModal.scss";
 
+type Props = {
+    slug: string,
+    columns: GridColDef[],
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
 
-
-function AddModal() {
-  return (
-    <div>AddModal</div>
-  )
+const AddModal = (props: Props) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //add new item
+        //axios post
+    }
+    return (
+        <div className="add">
+        <div className="modal">
+            <span className="close" onClick={() => props.setOpen(false)}>X</span>
+            <h1>Add New {props.slug}</h1>
+            <form onSubmit={handleSubmit}>
+                {props.columns.filter(item => item.field !== "id" && item.field !== "img")
+                    .map(column => (
+                        <div className="item">
+                            <label>{column.headerName}</label>
+                            <input type={column.type} />
+                        </div>
+                    ))}
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        </div>
+    )
 }
 
 export default AddModal
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,18 +1,13 @@
-// const rows: GridRowsProp = [
-//   //   { id: 1, col1: 'Hello', col2: 'World', status: true },
-//   //   { id: 2, col1: 'DataGridPro', col2: 'is Awesome', status: false },
-//   //   { id: 3, col1: 'MUI', col2: 'is Amazing', status: true },
-//   // ];
-
-
 import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable"
 import "./users.scss"
 import { userRows } from "../../data";
+import { useState } from "react";
+import AddModal from "../../components/addModal/AddModal";
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90},
-  { field: 'avatar', headerName: 'Avatar', width: 100, renderCell: (params) => {
+  { field: 'img', headerName: 'Avatar', width: 100, renderCell: (params) => {
     return <img src={params.row.img || "/noavatar.png"} alt="" />;
   }},
   { field: 'firstName',type:"string", headerName: 'First Name', width: 150, editable: true},
@@ -23,16 +18,17 @@ const columns: GridColDef[] = [
   { field: 'verified',type:"boolean", headerName: 'verified', width: 100},
 ];
 function Users() {
+  const [open, setOpen] = useState(false)
   return (
     <div className='user'>
       <div className="info">
         <div className="subInfo">
         <h1>Users</h1>
-        <button>Add New User</button>
-
+        <button onClick={()=> setOpen(true)}>Add New User</button>
         </div>
         <div className="table">
           <DataTable slug="users" columns={columns} rows={userRows} />
+          {open && <AddModal slug="user" columns={columns} setOpen={setOpen} />}
         </div>
       </div>
     </div>
